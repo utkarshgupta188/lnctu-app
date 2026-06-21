dc76f8e6c102f7a29f98699c162459b643addd06
utkarshgupta188 <guptautkarsh2312@outlook.com>
2026-06-21 18:05:31 +0530

    refactor: disable dependency metadata in build * exclude metadata from APKs * exclude metadata from Android App Bundles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,12 @@ android {
     buildFeatures {
         compose = true
     }
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
 }
 
 dependencies {
